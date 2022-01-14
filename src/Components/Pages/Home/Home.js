6852b86157c837e522adf4e790555b8aa1d6545d
mdAshik922 <md.ashik0005723@gmail.com>
2022-01-14 20:38:40 +0600

    commit

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,10 +1,7 @@
 import React from 'react';
-
 import Banner from '../HomeBanner/Banner';
 import Message from '../SendMessage/Message';
 import Service from '../Service/Service';
-
-
 
 const Home = () => {
     return (
