2062bbbc58c1d5b1ad43b1871369b4c427b6ddf5
mdAshik922 <md.ashik0005723@gmail.com>
2022-01-15 20:33:03 +0600

    commit

--- src/Components/Appointment/Appointment.js
+++ src/Components/Appointment/Appointment.js
@@ -5,8 +5,6 @@ import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
 import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import { Button} from '@mui/material';
 import { NavLink } from 'react-router-dom';
