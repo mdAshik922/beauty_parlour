f924997f15540adf743a40df06be621fb15473ec
mdAshik922 <md.ashik0005723@gmail.com>
2022-04-27 21:20:00 +0600

    commit

--- src/Components/Pages/Footer/Footer.js
+++ src/Components/Pages/Footer/Footer.js
@@ -22,7 +22,7 @@ H#000 (1st Floor), Road #00,
   </Grid>
                  <Grid item xs={4} md={6}>
                  <Typography variant="h6">
-magendo
+magend
          </Typography>
                  <Typography variant="h6">
 magendo
